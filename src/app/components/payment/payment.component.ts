3386d3588d2c8ea211d1f3559f6c7027e9ffd144
Kitipoom <poom10510@gmail.com>
2017-08-08 18:15:58 +0700

    update report

--- src/app/components/payment/payment.component.ts
+++ src/app/components/payment/payment.component.ts
@@ -4,6 +4,7 @@ import { OrderdetailService } from '../../services/orderdetail.service';
 import { AppComponent } from '../../app.component';
 import * as FileSaver from 'file-saver';
 import * as XLSX from 'xlsx';
+import { Angular2Csv } from 'angular2-csv/Angular2-csv';
 
 const EXCEL_TYPE = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8';
 const EXCEL_EXTENSION = '.xlsx';
@@ -23,7 +24,7 @@ export class PaymentComponent implements OnInit {
 
     ngOnInit() {
         if (sessionStorage.getItem('createorderresult') != null) {
-            if(localStorage.getItem('offlinereport')!=null){
+            if (localStorage.getItem('offlinereport') != null) {
                 this.dayreport = JSON.parse(localStorage.getItem('offlinereport'));
             }
             this.paymentdetail = JSON.parse(sessionStorage.getItem('createorderresult'));
@@ -76,18 +77,55 @@ export class PaymentComponent implements OnInit {
 
         var blob = new Blob([ str], {type: "text/plain;charset=utf-8"});
         FileSaver.saveAs(blob, "hello world.txt");*/
-        
-       
-        this.exportAsExcelFile(this.dayreport, "report");
+
+        this.csvt();
+        //this.exportAsExcelFile(this.dayreport, "report");
+    }
+
+    createreport() {
+        //console.log(this.orderdetail.customerId);
+        var nowreport = {
+            "masterOrderId": this.orderdetail.masterOrderId,
+            "shippingAddress": this.orderdetail.shippingAddress,
+            "billingAddress": this.orderdetail.billingAddress,
+            "currencyCode": this.orderdetail.currencyCode,
+            "customerEmailAddress": this.orderdetail.customerEmailAddress,
+            "customerId": this.orderdetail.customerId,
+            "customerName": this.orderdetail.customerName,
+            "masterOrderNo": this.orderdetail.masterOrderNo,
+            "orderDateTime": this.orderdetail.orderDateTime,
+            "status": this.orderdetail.status,
+            "isMediaOrder": this.orderdetail.isMediaOrder,
+            "masterOrderSubTotal": this.orderdetail.masterOrderSubTotal,
+            "masterOrderTotal": this.orderdetail.masterOrderTotal,
+            "masterOrderTotalDiscount": this.orderdetail.masterOrderTotalDiscount,
+            "masterOrderTotalShipping": this.orderdetail.masterOrderTotalShipping,
+            "masterOrderTotalVat": this.orderdetail.masterOrderTotalShipping,
+            "masterOrderTotalAmount": this.orderdetail.masterOrderTotalAmount,
+            "createDateTime": this.orderdetail.createDateTime,
+            "updateDateTime": this.orderdetail.updateDateTime,
+            "shippingAddressId": this.orderdetail.shippingAddressId,
+            "time": new Date().getDate() + "-" + (new Date().getMonth() + 1) + "-" + new Date().getFullYear() + "-" + new Date().getHours() + "-" + new Date().getMinutes() + "-" + new Date().getSeconds(),
+        }
+        this.dayreport.push(nowreport);
+        localStorage.setItem('offlinereport', JSON.stringify(this.dayreport));
+    }
+    csvt() {
+        //new Angular2Csv(data, 'My Report');
+        var options = {
+            fieldSeparator: ',',
+            quoteStrings: '"',
+            decimalseparator: '.',
+            showLabels: true,
+            showTitle: true,
+            useBom: true
+        };
+        var name = "My Report" + new Date().getDate() + "_" + (new Date().getMonth() + 1) + "_" + new Date().getFullYear()+"_"+new Date().getHours() + "_" + new Date().getMinutes();
+        new Angular2Csv(this.dayreport,name , options);
     }
     public exportAsExcelFile(json: any[], excelFileName: string): void {
         if (json != null) {
-            //console.log(JSON.stringify(json));
-            
             var b = json;
-            //b =[{"b":1},{"b":2}];
-            //console.log(JSON.stringify(b));
-            //a.push(JSON.stringify(b));
             const worksheet: XLSX.WorkSheet = XLSX.utils.json_to_sheet(b);
             const workbook: XLSX.WorkBook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
             const excelBuffer: any = XLSX.write(workbook, { bookType: 'xlsx', type: 'buffer' });
@@ -100,58 +138,9 @@ export class PaymentComponent implements OnInit {
         const data: Blob = new Blob([buffer], {
             type: EXCEL_TYPE
         });
-        FileSaver.saveAs(data, fileName + '_export_' + new Date().getTime() + EXCEL_EXTENSION);
+        FileSaver.saveAs(data, fileName + '_export_' +  new Date().getDate() + "_" + (new Date().getMonth() + 1) + "_" + new Date().getFullYear()+"_"+new Date().getHours() + "_" + new Date().getMinutes() + EXCEL_EXTENSION);
     }
-    createreport(){
-        //console.log(this.orderdetail.customerId);
-        var nowreport = {
-                "masterOrderId": this.orderdetail.masterOrderId,
-                "shippingAddress": this.orderdetail.shippingAddress,
-                "billingAddress":this.orderdetail.billingAddress,
-                "currencyCode": this.orderdetail.currencyCode,
-                "customerEmailAddress": this.orderdetail.customerEmailAddress,
-                "customerId":this.orderdetail.customerId,
-                "customerName": this.orderdetail.customerName,
-                "masterOrderNo": this.orderdetail.masterOrderNo,
-                "orderDateTime": this.orderdetail.orderDateTime,
-                "status":this.orderdetail.status,
-                "isMediaOrder":this.orderdetail.isMediaOrder,
-                "masterOrderSubTotal":this.orderdetail.masterOrderSubTotal,
-                "masterOrderTotal":this.orderdetail.masterOrderTotal,
-                "masterOrderTotalDiscount":this.orderdetail.masterOrderTotalDiscount,
-                "masterOrderTotalShipping":this.orderdetail.masterOrderTotalShipping,
-                "masterOrderTotalVat": this.orderdetail.masterOrderTotalShipping,
-                "masterOrderTotalAmount":this.orderdetail.masterOrderTotalAmount,
-                "createDateTime":this.orderdetail.createDateTime,
-                "updateDateTime":this.orderdetail.updateDateTime,
-                "shippingAddressId":this.orderdetail.shippingAddressId,
-        }
-        this.dayreport.push(nowreport);
-         localStorage.setItem('offlinereport', JSON.stringify(this.dayreport));
-    }
+
+
 
 }
-
- /* if(sessionStorage.getItem('cart')!=null&&sessionStorage.getItem('cartorder')!=null){
-            sessionStorage.removeItem('cart');
-            sessionStorage.removeItem('cartorder');
-            this.app.checkCartvalue();
-            var listorder = JSON.parse(sessionStorage.getItem('cartcreateorder'));
-            this.createorderService.list(listorder)
-                .then((data: any) => {
-                if (data.result == "SUCCESS") {
-                   this.sendedorder = data.data;
-                      sessionStorage.setItem('createorderresult',JSON.stringify(this.sendedorder));
-                      this.paymentdetail = this.sendedorder;
-                }
-            });
-        }
-        else{
-            if(sessionStorage.getItem('createorderresult')!=null){
-                this.paymentdetail = JSON.parse(sessionStorage.getItem('createorderresult'));
-                console.log("refresh");
-            }
-            else{
-                this.router.navigate(['/']);
-            }
-        }*/
