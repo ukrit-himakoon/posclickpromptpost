c9e40f41709750e8d31e32424814d32c3f3a0005
Kitipoom <poom10510@gmail.com>
2017-08-09 18:37:17 +0700

    Test address format

--- src/app/components/MediaPlayer/media.component.ts
+++ src/app/components/MediaPlayer/media.component.ts
@@ -12,15 +12,23 @@ export class MediaComponent implements OnInit {
     products: any[];
     orderproducts: any[];
     priceorder: any = {};
-    numproduct= 0;
+    numproduct = 0;
 
     //constructor(private router: Router) { }
 
-    constructor(private router: Router,private app:AppComponent) {}
+    constructor(private router: Router, private app: AppComponent) { }
 
+    textresult = 'gg';
     ngOnInit() {
 
-        
-        
-    } 
+
+
+    }
+    generateText(f: string, l: string, ad: string, ar: string, ci: string, co: string, zi: string, tel: string) {
+        if (f && l && ad && ar && ci && co && zi && tel) {
+            var Addressd = f + " " + l + " " + ad + " " + ar + " " + ci + " " + co + " " + zi + " " + tel;
+            console.log(Addressd);
+            this. textresult = Addressd;
+        }
+    }
 }
